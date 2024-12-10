d8d322543c1c405533d1b3f30565a22e40736f46
yuyanjun <yuyanjun@zego.im>
2024-12-10 16:19:41 +0800

    add data report

---
+++ lib/src/internal/reporter.dart
@@ -0,0 +1,25 @@
+import 'package:zego_uikit/zego_uikit.dart';
+
+class ZegoAudioRoomReporter {
+  static String eventInit = "liveaudioroom/init";
+  static String eventUninit = "liveaudioroom/unInit";
+
+  /// Version number of each kit, usually in three segments
+  static String eventKeyKitVersion = "liveaudioroom_version";
+
+  Future<void> report({
+    required String event,
+    Map<String, Object> params = const {},
+  }) async {
+    ZegoUIKit().reporter().report(event: event, params: params);
+  }
+
+  factory ZegoAudioRoomReporter() {
+    return instance;
+  }
+
+  ZegoAudioRoomReporter._internal();
+
+  static final ZegoAudioRoomReporter instance =
+      ZegoAudioRoomReporter._internal();
+}
